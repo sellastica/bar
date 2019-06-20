bc500a227852f4da1834ebf1ac75a92f01c0f39d
Tomáš Krejčí <tomas@differencemakers.com>
2019-06-20 09:02:59 +0200

    .

--- src/Component/Bar.php
+++ src/Component/Bar.php
@@ -10,10 +10,15 @@ class Bar extends \Sellastica\AdminUI\Component\BaseControl
 	/**
 	 * @param string $title
 	 * @param string $url
+	 * @param bool $openInNewWindow
 	 */
-	public function addLink(string $title, string $url)
+	public function addLink(
+		string $title,
+		string $url,
+		bool $openInNewWindow = false
+	)
 	{
-		$this->links[] = [$title, $url];
+		$this->links[] = [$title, $url, $openInNewWindow];
 	}
 
 	/**
